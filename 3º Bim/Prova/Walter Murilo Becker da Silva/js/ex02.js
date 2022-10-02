ca5fd18760d418ac874ed06692d59f47dbcbb1d3
Murilo <64659295+wmbeckers@users.noreply.github.com>
2022-10-02 17:26:05 -0400

    a

--- 3º Bim/Prova/Walter Murilo Becker da Silva/js/ex02.js
+++ 3º Bim/Prova/Walter Murilo Becker da Silva/js/ex02.js
@@ -1,8 +1,8 @@
 function ex2() {
 
-const num1 = parseFloat(prompt("Insira o primeiro número: "));
-const num2 = parseFloat(prompt("Insira o segundo número: "));
-const num3 = parseFloat(prompt("Insira o terceiro número: "));
+const num1 = parseInt(prompt("Insira o primeiro número: "));
+const num2 = parseInt(prompt("Insira o segundo número: "));
+const num3 = parseInt(prompt("Insira o terceiro número: "));
 let maior;
 
 if(num1 >= num2 && num1 >= num3) {
--- 3º Bim/Prova/Walter Murilo Becker da Silva/js/ex04.js
+++ 3º Bim/Prova/Walter Murilo Becker da Silva/js/ex04.js
@@ -1,1 +1,82 @@
-
+function ex4(){
+        var n1;
+        var numero1=prompt ("Insira o primeiro número");
+            if (numero1!=null) {
+                n1 = ("O quadrado deste número é " + numero1**2 + "!");
+    } else {
+        n1 = ("Você deve inserir um número!")
+    }
+    var n2;
+    var numero2=prompt ("Insira o segundo número");
+        if (numero2!=null) {
+            n2 = ("O quadrado deste número é " + numero2**2 + "!");
+} else {
+    n2 = ("Você deve inserir um número!")
+}
+var n3;
+var numero3=prompt ("Insira o terceiro número");
+    if (numero3!=null) {
+        n3 = ("O quadrado deste número é " + numero3**2 + "!");
+} else {
+n3 = ("Você deve inserir um número!")
+}
+var n4;
+var numero4=prompt ("Insira o quarto número");
+    if (numero4!=null) {
+        n4 = ("O quadrado deste número é " + numero4**2 + "!");
+} else {
+n4 = ("Você deve inserir um número!")
+}
+var n5;
+var numero5=prompt ("Insira o quinto número");
+    if (numero5!=null) {
+        n5 = ("O quadrado deste número é " + numero5**2 + "!");
+} else {
+n5 = ("Você deve inserir um número!")
+}
+var n6;
+var numero6=prompt ("Insira o sexto número");
+    if (numero6!=null) {
+        n6 = ("O quadrado deste número é " + numero6**2 + "!");
+} else {
+n6 = ("Você deve inserir um número!")
+}
+var n7;
+var numero7=prompt ("Insira o sétimo número");
+    if (numero7!=null) {
+        n7 = ("O quadrado deste número é " + numero7**2 + "!");
+} else {
+n7 = ("Você deve inserir um número!")
+}
+var n8;
+var numero8=prompt ("Insira o oitavo número");
+    if (numero8!=null) {
+        n8 = ("O quadrado deste número é " + numero8**2 + "!");
+} else {
+n8 = ("Você deve inserir um número!")
+}
+var n9;
+var numero9=prompt ("Insira o nono número");
+    if (numero9!=null) {
+        n9 = ("O quadrado deste número é " + numero9**2 + "!");
+} else {
+n9 = ("Você deve inserir um número!")
+}
+var n10;
+var numero10=prompt ("Insira o décimo número");
+    if (numero10!=null) {
+        n10 = ("O quadrado deste número é " + numero10**2 + "!");
+} else {
+n10 = ("Você deve inserir um número!")
+}
+document.getElementById("sq1").innerHTML=n1;
+document.getElementById("sq2").innerHTML=n2;
+document.getElementById("sq3").innerHTML=n3;
+document.getElementById("sq4").innerHTML=n4;
+document.getElementById("sq5").innerHTML=n5;
+document.getElementById("sq6").innerHTML=n6;
+document.getElementById("sq7").innerHTML=n7;
+document.getElementById("sq8").innerHTML=n8;
+document.getElementById("sq9").innerHTML=n9;
+document.getElementById("sq10").innerHTML=n10;
+}
